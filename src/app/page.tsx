5e214d11141fee31eec43ab785bc2fd2005622de
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2023-08-24 10:57:03 -0300

    completed OurAlliances section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { BusinessInsight, FeacturedService, FrequentQuestions, FrontPage } from "./components";
+import { BusinessInsight, FeacturedService, FrequentQuestions, FrontPage, OurAlliances } from "./components";
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
       <FeacturedService />
       <BusinessInsight />
       <FrequentQuestions />
+      <OurAlliances />
     </>
   )
 }
