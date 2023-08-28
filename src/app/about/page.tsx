88787319136d1f9314eb7488813d4dce8b0aebb1
gastonig9 <gastonig2020@gmail.com>
2023-08-28 11:23:31 -0300

    add Founder component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,13 @@
 import style from './about.module.css'
 import Banner from './components/Banner/BannerAbout'
+import Founders from './components/Founders/Founders'
+
 
 export default function About(){
     return(
         <section className={style.aboutContainer}>
             <Banner/>
+            <Founders/>
         </section>
     )
 }
