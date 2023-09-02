8b97708e64f88157b9b934ea297e9adf08ad0dbe
samuel carrizo <samuelcarrizot@gmail.com>
2023-09-02 17:28:57 -0300

    cases responsive and padding banner about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,11 @@
-/* import vector3 from '../../assets/images/vector3.svg'
-import vector1 from '../../assets/images/OBJECTS (1).svg'
-import Image from 'next/image' */
 import { BannerAbout, Founders, Mvv, Team } from './components'
+import OurFounders from './components/OurFounders/OurFounders'
 import Trajectory from './components/Trajectory/Trajectoty'
 
 
 export default function About() {
   return (
     <>
-      {/* <Image className={style.vector3} alt='vector2' src={vector3} />
-      <Image className={style.vector1} alt='vector1' src={vector1} /> */}
       <BannerAbout />
       <Founders />
       <Mvv />
