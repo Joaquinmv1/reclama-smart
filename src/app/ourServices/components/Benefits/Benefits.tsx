9b4576a1c07f417d24d93f48f34d952518bb7131
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2023-09-05 21:45:10 -0300

    correction home

--- src/app/ourServices/components/Benefits/Benefits.tsx
+++ src/app/ourServices/components/Benefits/Benefits.tsx
@@ -2,25 +2,21 @@ import { benefitsData } from "@/assets/constants/constants";
 import Image from "next/image";
 import style from './Benefits.module.css';
 import Button from "@/components/Button/Button";
+import tilde from '../../../../assets/images/tilde.jpg';
 
 function Benefits() {
   return (
     <>
       <section className={style.containerBenefits}>
-<<<<<<< HEAD
-        <h2>¡Conoce nuestros servicios!</h2>
-=======
-        {/* <p>Nuestros Servicios</p>
-        <h2>Te ofrecemos un conjunto completo de servicios diseñados para proteger tus derechos. Nuestro equipo de abogados experimentados esta aquí para respaldarte en una variedad de situaciones.</h2> */}
->>>>>>> 3572e7c92e08f3d86740efeef185754f2989da4c
         <div className={style.cardBenefits}>
+          <h2>¡Conoce nuestros servicios!</h2>
           <ul className={style['benefits-card-container']}>
             {benefitsData.map((card, i) => {
               return (
                 <li key={`${card.image} ${i}`}>
                   <Image width={60} src={card.image} alt={`${card.title} services`} />
                   <h3>{card.title}</h3>
-                  {card.benefits.map((benefit, index) => <p key={`${card.benefits} ${index}`}>✔️ {benefit}</p>)}
+                  {card.benefits.map((benefit, index) => <p key={`${card.benefits} ${index}`}> <Image src={''} alt="tilde"/> {benefit}</p>)}
                 </li>
               )
             })}
