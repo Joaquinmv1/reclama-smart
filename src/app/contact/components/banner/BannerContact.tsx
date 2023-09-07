930b54daf29ab2b578c7c668ca97eec2d16b4e41
frankYanez <frankyanez9413@gmail.com>
2023-09-07 09:34:34 -0300

    Contact page finished

--- src/app/contact/components/banner/BannerContact.tsx
+++ src/app/contact/components/banner/BannerContact.tsx
@@ -6,45 +6,56 @@ import calendlyImg from '../../../../assets/images/calendly-img.png';
 export default function BannerContact() {
     return (
         <section className={style.bannerSection}>
-            <aside className={style.containerBody}>
+            <aside className={style.titles}>
 
                 <h2 className={style.title}>Estamos a tu <span>disposicion</span> </h2>
                 <h3>Programa una reunión <span>gratis</span> </h3>
-                <div className={style.content}>
-                    <div className={style.logoStep}>
-                        <h3>1</h3>
+            </aside>
+            <aside className={style.containerBody}>
+
+
+                <section className={style.containerSteps}>
+
+
+
+                    <div className={style.content}>
+
+                        <div className={style.logoStep}>
+                            <h3>1</h3>
+                        </div>
+                        <aside>
+                            <h4>Selecciona tu horario</h4>
+                            <p>Descubre las opciones en nuestro calendario de disponibilidad</p>
+                        </aside>
                     </div>
-                    <aside>
-                        <h4>Selecciona tu horario</h4>
-                        <p>Descubre las opciones en nuestro calendario de disponibilidad</p>
-                    </aside>
 
-                </div>
-                <div className={style.content}>
-                    <div className={style.logoStep}>
-                        <h3>2</h3>
+
+                    <div className={style.content}>
+                        <div className={style.logoStep}>
+                            <h3>2</h3>
+                        </div>
+                        <aside>
+
+                            <h4>Completa la información</h4>
+                            <p>Empecemos por conocerte mejor.Indícanos tu nombre, correo electrónico y cualquier información que consideres relevante que sepamos.</p>
+                        </aside>
                     </div>
-                    <aside>
+                    <div className={style.content}>
 
-                        <h4>Completa la información</h4>
-                        <p>Empecemos por conocerte mejor.Indícanos tu nombre, correo electrónico y cualquier información que consideres relevante que sepamos.</p>
-                    </aside>
-                </div>
-                <div className={style.content}>
-
-                    <div className={style.logoStep}>
-                        <h3 >3</h3>
+                        <div className={style.logoStep}>
+                            <h3 >3</h3>
+                        </div>
+                        <aside>
+                            <h4>Confirmación y recordatorio:</h4>
+                            <p>Una vez agendada tu cita, recibirás una confirmación automática.</p>
+                        </aside>
                     </div>
-                    <aside>
-                        <h4>Confirmación y recordatorio:</h4>
-                        <p>Una vez agendada tu cita, recibirás una confirmación automática.</p>
-                    </aside>
-                </div>
+                    <Button title={"Agendar reunion"} className={style.button} />
+                </section>
 
-                <Button title={"Agendar reunion"} className={style.button} />
+                <article><Image className={style.calendlyImg} src={calendlyImg} alt='Calendly image' /></article>
             </aside>
 
-            <article><Image className={style.calendlyImg} src={calendlyImg} alt='Calendly image' /></article>
         </section>
     )
 }
