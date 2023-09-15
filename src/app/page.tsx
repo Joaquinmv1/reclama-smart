ddfaff78656f5d0fd1ba543a254679821deb4985
Joaquinmv1 <villarrealjoaquin09@gmail.com>
2023-09-15 02:06:49 -0300

    added latest changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import { BusinessInsight, Cases, FeacturedService, FrequentQuestions, FrontPage, OurAlliances } from "./components";
 import { dataClients } from "@/assets/constants/constants";
+import { BusinessInsight, Cases, FeacturedService, FrequentQuestions, FrontPage, OurAlliances } from "./components";
 import './globals.css';
 
 export default function Home() {
   return (
     <main>
-        <FrontPage />
-        <FeacturedService />
-        <BusinessInsight />
-        <Cases data={dataClients} />
-        <OurAlliances />
-        <FrequentQuestions />
+      <FrontPage />
+      <FeacturedService />
+      <BusinessInsight />
+      <Cases data={dataClients} />
+      <OurAlliances />
+      <FrequentQuestions />
     </main>
   )
 }
--- src/app/terms/components/Segments/Segments.tsx
+++ src/app/terms/components/Segments/Segments.tsx
@@ -5,38 +5,38 @@ import arrow from "../../../../assets/images/chevron-up.png";
 import Image from "next/image";
 
 export default function Segments() {
-    const [open, setOpen] = useState<number | null>(null)
+  const [open, setOpen] = useState<number | null>(null)
 
-    const handleOpen = (index: number) => {
-        setOpen(index == open ? null : index)
-    }
+  const handleOpen = (index: number) => {
+    setOpen(index == open ? null : index)
+  }
 
-    return (
-        <section className={style.containerGeneral}>
+  return (
+    <section className={style.containerGeneral}>
+      {
+        terms.map((term, index) => (
+          <div className={style.container} key={term.title}>
+            <div className={style.title} onClick={() => handleOpen(index)}>
+              {
+                term.segment
+                  ? <Image src={arrow} alt="arrow" className={`${style.arrow} ${open === index ? style.rotated : ""}`} />
+                  : <small className={style.margin}></small>
+              }
+              <h3>{term.title}</h3>
+            </div>
             {
-                terms.map((term, index) => (
-                    <div className={style.container} key={term.title}>
-                        <div className={style.title} onClick={() => handleOpen(index)}>
-                            {
-                                term.segment ? <Image src={arrow} alt="arrow" className={`${style.arrow} ${open === index ? style.rotated : ""}`} /> : <small className={style.margin}></small>
-                            }
-                            <h3>{term.title}</h3>
-                        </div>
-                        {
-                            open === index && (
-                                <div className={`${style.segment} ${open === index ? style.open : ""}`}>
-                                    {term.segment?.map((segment, index) => (
-                                        <p key={index}>{segment}</p>
-                                    ))}
+              open === index && term.segment &&  (
+                <div className={`${style.segment} ${open === index ? style.open : ""}`}>
+                  {term.segment?.map((segment, index) => (
+                    <p key={index}>{segment}</p>
+                  ))}
+                </div>
 
-                                </div>
-
-                            )
-                        }
-                    </div>
-                ))
+              )
             }
-
-        </section>
-    )
+          </div>
+        ))
+      }
+    </section>
+  )
 }
